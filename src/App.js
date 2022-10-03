d5d8059c19f945efe5e7b9d86e6b8a594053fe28
David Chedrick <davidchedrick@gmail.com>
2022-10-03 10:48:07 -0500

    added profile glla

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import "./App.css";
 import { PRODUCTS } from "./features/food/fruitData";
 import ProductsArea from "./features/food/ProductsArea";
-import "./App.css";
+import ProfileArea from "./features/profile/ProfileArea";
 
 function App() {
 	return (
 		<div className="App">
+			<ProfileArea />
 			<ProductsArea products={PRODUCTS} />
 		</div>
 	);
---
+++ src/features/profile/ProfileArea.js
@@ -0,0 +1,14 @@
+import Profile from "./Profile";
+
+const ProfileArea = () => {
+	return (
+		<section>
+			<h1>Amazing scientists</h1>
+			<Profile />
+			<Profile />
+			<Profile />
+		</section>
+	);
+};
+
+export default ProfileArea;
