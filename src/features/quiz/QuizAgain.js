973168b05cff17c68de7095eee3edeec9f765d77
David Chedrick <davidchedrick@gmail.com>
2022-10-29 12:30:05 -0500

    updated quiz app

--- src/features/quiz/QuizAgain.js
+++ src/features/quiz/QuizAgain.js
@@ -1,52 +1,54 @@
 import { useEffect, useState } from "react";
+const data = [
+    {
+        question: "Which of the following is a built-in React hook?",
+        answers: ["useState", "useFetch", "useLocalStorage", "useTimeout"],
+        correctAnswer: 0,
+    },
+    {
+        question: "What is the correct order of these lifecycle phases?",
+        answers: [
+            "unmount, update, mount",
+            "mount, update, unmount",
+            "update, mount, unmount",
+            "mount, unmount, update",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "What is reconciliation?",
+        answers: [
+            "The process of converting the virtual DOM to DOM nodes.",
+            "The process of combining two virtual DOM trees into one.",
+            "The algorithm used by React to determine which state updates to batch together.",
+            "The algorithm used by React to determine which elements changed between renders.",
+        ],
+        correctAnswer: 3,
+    },
+];
 
 function QuizAgain() {
-    const [questions, setQuestions] = useState({});
+    const [questions, setQuestions] = useState(null);
+    console.log("questions: ", questions);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedAnswers, setSelectedAnswers] = useState([]);
+    const [chosenAnswers, setChosenAnswers] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-            const data = [
-                {
-                    question:
-                        "Which of the following is a built-in React hook?",
-                    answers: [
-                        "useState",
-                        "useFetch",
-                        "useLocalStorage",
-                        "useTimeout",
-                    ],
-                    correctAnswer: 0,
-                },
-                {
-                    question:
-                        "What is the correct order of these lifecycle phases?",
-                    answers: [
-                        "unmount, update, mount",
-                        "mount, update, unmount",
-                        "update, mount, unmount",
-                        "mount, unmount, update",
-                    ],
-                    correctAnswer: 1,
-                },
-                {
-                    question: "What is reconciliation?",
-                    answers: [
-                        "The process of converting the virtual DOM to DOM nodes.",
-                        "The process of combining two virtual DOM trees into one.",
-                        "The algorithm used by React to determine which state updates to batch together.",
-                        "The algorithm used by React to determine which elements changed between renders.",
-                    ],
-                    correctAnswer: 3,
-                },
-            ];
+            // const response = await fetch(data);
+            // const json = await response.json();
             setQuestions(data);
         };
         fetchData();
     }, []);
 
-    if (!questions) return null;
+    if (questions == null) return null;
+
+    const updateChosenAnswers = (questionIndex, answerIndex) => {
+        const newChosenAnswers = [...chosenAnswers];
+        newChosenAnswers[questionIndex] = answerIndex;
+        setChosenAnswers(newChosenAnswers);
+    };
 
     const currentQuestion = questions[currentQuestionIndex];
     const isFirstQuestion = currentQuestionIndex === 0;
@@ -54,39 +56,34 @@ function QuizAgain() {
 
     return (
         <>
-            <h1>cactcatctatcatcactatcatca!!!!8888877!!!</h1>
             <h1>{currentQuestion.question}</h1>
-
             {currentQuestion.answers.map((answer, answerIndex) => {
+                const chosenAnswer = chosenAnswers[currentQuestionIndex];
                 let className = "answer";
-                const selectedAnswer = selectedAnswers[currentQuestionIndex];
 
-                // if (selectedAnswer === answerIndex) {
-                //     className +=
-                //         currentQuestion.correctAnswer === selectedAnswer
-                //             ? " correct"
-                //             : " incorrect";
-                // }
+                if (chosenAnswer === answerIndex) {
+                    className +=
+                        currentQuestion.correctAnswer === chosenAnswer
+                            ? " correct"
+                            : " incorrect";
+                }
 
-                // return (
-                //     <>
-                //         <h2
-                //             key={answerIndex}
-                //             className={className}
-                //             onClick={() => {
-                //                 if (!selectedAnswer)
-                //                     setSelectedAnswers([
-                //                         ...selectedAnswer,
-                //                         answerIndex,
-                //                     ]);
-                //             }}
-                //         >
-                //             {answer}
-                //         </h2>
-                //     </>
-                // );
+                return (
+                    <h2
+                        key={answer}
+                        className={className}
+                        onClick={() => {
+                            if (chosenAnswer != null) return;
+                            updateChosenAnswers(
+                                currentQuestionIndex,
+                                answerIndex
+                            );
+                        }}
+                    >
+                        {answer}
+                    </h2>
+                );
             })}
-
             <button
                 disabled={isFirstQuestion}
                 onClick={() =>
@@ -96,7 +93,10 @@ function QuizAgain() {
                 Back
             </button>
             <button
-                disabled={isLastQuestion}
+                disabled={
+                    isLastQuestion ||
+                    chosenAnswers[currentQuestionIndex] == null
+                }
                 onClick={() =>
                     setCurrentQuestionIndex(currentQuestionIndex + 1)
                 }
